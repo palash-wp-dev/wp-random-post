c2c52a44ea1ef4ec38bcb73e38e11f03cebe1848
palash-wp-dev <raps.palash.840@gmail.com>
2025-03-20 16:22:51 +0600

    Made some changes

--- ajax.js
+++ ajax.js
@@ -1,1 +1,9 @@
-()()
+(function($){
+    $(document).ready(function(){
+        $("#clickMe").click(function(){
+            $.ajax(function(){
+                
+            });
+        });
+    });
+})(jQuery)
--- wp-random-post.php
+++ wp-random-post.php
@@ -12,3 +12,36 @@ function file_enquetion()
 }
 add_action( 'wp_enqueue_scripts', 'file_enquetion' );
 
+// ajax call
+function random_post()
+{
+    $args = [
+        'post_type' => 'post',
+        'orderby' => 'rand'
+        'posts_per_page' => 1,
+    ];
+
+    $query = new WP_Query( $args );
+
+    if ( $query->have_posts() ) {
+        while( $query->have_posts() ) {
+            $query->the_post();
+
+            echo $query->the_title();
+        }
+    } else {
+        echo esc_html__( 'No post found', 'wp-random-post' )
+    }
+}
+add_action( 'wp_ajax_random_post', 'random_post' );
+add_action( 'wp_ajax_nopriv_random_post', 'random_post' );
+
+function add_button()
+{
+    ?>
+    <input type="button" id="clickMe" value="Click">
+    <?php 
+}
+
+add_shortcode( 'button', 'add_button' );
+
